9fe9a2b3d9303324448de3bd167932bb78eadda5
ENSEMUQAC <pierremasquilier@gmail.com>
2017-11-13 14:18:24 -0500

    Commit appli à jour

--- app/src/main/java/com/masquilierpemeja/rugbyfantasytop14/MenuPrincipal/MenuPrincipalActivity.java
+++ app/src/main/java/com/masquilierpemeja/rugbyfantasytop14/MenuPrincipal/MenuPrincipalActivity.java
@@ -1,12 +1,16 @@
 package com.masquilierpemeja.rugbyfantasytop14.MenuPrincipal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import com.google.firebase.auth.FirebaseAuth;
 import com.masquilierpemeja.rugbyfantasytop14.CreerChampionnat.CreerChampionnatActivity;
+import com.masquilierpemeja.rugbyfantasytop14.Login.LoginActivity;
+import com.masquilierpemeja.rugbyfantasytop14.MenuPrincipal.MenuPrincipalPresenterImpl;
 import com.masquilierpemeja.rugbyfantasytop14.MenuPrincipal.MenuPrincipalView;
 import com.masquilierpemeja.rugbyfantasytop14.Profil.ProfilActivity;
 import com.masquilierpemeja.rugbyfantasytop14.R;
@@ -15,11 +19,15 @@ import com.masquilierpemeja.rugbyfantasytop14.RejoindreChampionnat.RejoindreCham
 public class MenuPrincipalActivity extends AppCompatActivity implements MenuPrincipalView {
 
     Button btnGoToMonProfil, btnGoToCreerChampionnat, btnGoToRejoindreChampionnat, btnDeconnexion;
+    private MenuPrincipalPresenterImpl mMenuPrincipalPresenterImpl;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         btnGoToMonProfil = (Button) findViewById(R.id.btn_go_to_mon_profil);
         btnGoToCreerChampionnat = (Button) findViewById(R.id.btn_go_to_creer_championnat);
@@ -40,6 +48,19 @@ public class MenuPrincipalActivity extends AppCompatActivity implements MenuPrin
             }
         });
 
+        btnDeconnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                mFirebaseAuth.signOut();
+                startActivity(new Intent(MenuPrincipalActivity.this, LoginActivity.class));
+                finish();
+            }
+
+
+    });
+
         btnGoToMonProfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +68,12 @@ public class MenuPrincipalActivity extends AppCompatActivity implements MenuPrin
             }
         });
 
+    }
 
+
+    public MenuPrincipalPresenterImpl getMenuPrincipalPresenterImpl() {
+        if (mMenuPrincipalPresenterImpl == null) mMenuPrincipalPresenterImpl = new MenuPrincipalPresenterImpl(this);
+        return mMenuPrincipalPresenterImpl;
     }
 
     @Override
