777ac3e518b25b7090a506af3430c3d13dda734a
ENSEMUQAC <pierremasquilier@gmail.com>
2017-12-02 16:14:55 -0500

    DATABASE MANAGER : AJOUTER GETTER ET SETTER DE USER DANS FIREBASE

--- app/src/main/java/com/masquilierpemeja/rugbyfantasytop14/NoActivityClassPackage/DatabaseManager.java
+++ app/src/main/java/com/masquilierpemeja/rugbyfantasytop14/NoActivityClassPackage/DatabaseManager.java
@@ -1,8 +1,16 @@
 package com.masquilierpemeja.rugbyfantasytop14.NoActivityClassPackage;
 
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.google.android.gms.tasks.Task;
+
 
 /**
  * Created by Pierre on 02/12/2017.
@@ -28,7 +36,7 @@ public class DatabaseManager {
 
 
     /////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////CONSTRUCTEURs//////////////////////////////////////////////////
+    ///////////////////////CONSTRUCTEURS//////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -43,9 +51,35 @@ public class DatabaseManager {
     /////////////////////////////////////////////////////////////////////////////////////
 
 
-    
+    public void getUser(final String uid, final Result<User> result)
+    {
+        ref.users.child(uid).addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                user = dataSnapshot.getValue(User.class);
+                if (user != null) {
+                    result.onSuccess(user.setUserID(uid));
+
+                } else {
+                    result.onFailure();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+                result.onError(databaseError);
+            }
+        });
+    }
 
 
+    public Task<Void> setUser(final User user)
+    {
+        return ref.users.child(user.getUserID()).setValue(user);
+    }
 
 
 
@@ -76,6 +110,25 @@ public class DatabaseManager {
             database = FirebaseDatabase.getInstance().getReference();
             users = database.child(USERS);
         }
+    }
+
+
+
+    public static abstract class Result<T>
+    {
+        public void onComplete() {}
+
+        public abstract void onSuccess(T t);
+
+        public void onFailure()
+        {
+            onError(DatabaseError.fromException(new Exception("Data not found")));
+        }
+
+        public void onError(DatabaseError error)
+        {
+            Log.d("DATABASE ERROR", error.getMessage());
+        };
     }
 
 
